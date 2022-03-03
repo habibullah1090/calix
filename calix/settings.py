91ec02698d3000b134cfccbc7c9e0c6cb800c19c
habibullah1090 <habibullah.dewanbd@gmail.com>
2022-03-03 19:43:22 +0600

    Settings updated according to heroku doc

--- calix/settings.py
+++ calix/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -207,3 +207,7 @@ CKEDITOR_CONFIGS = {
         ]),
     }
 }
+
+
+
+django_heroku.settings(locals())
